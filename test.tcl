709ab45cb1db56a230ff4c07a2bce57591f8ec5f
Ben Stahl <48197017+benastahl@users.noreply.github.com>
2025-01-11 00:47:55 -0500

    Update test.tcl

--- test.tcl
+++ test.tcl
@@ -1,56 +1,20 @@
 load ./build/libtclslang.so
 
-set tree [slang_parse "test.sv" "test2.sv"]
+set tree [slang_parse "./verilog_tests/test5.v"]
 
-puts $tree
-
-set module [$tree get_module "memory"]
-set module2 [$tree get_module "memory2"]
-
-puts $module
-puts $module2
-
-puts [$module name]
-puts [$module2 name]
-
+set module [$tree get_module "example_module"]
 
 set ports [$module get_ports]
 
 puts "\n\n"
-puts "module1 ports!"
+puts "module ports!"
 puts $ports
 foreach p $ports {
-    puts "full port string: [$p string]"
-    puts "port [$p portType] [$p direction] [$p dataType]"
-    set ident_list [$p identifiers]
-
+    puts "port name: [$p name]"
+    puts "port type: [$p portType]"
+    puts "port direction: [$p direction]"
+    puts "port declared type: [$p type]"
+    puts "port dimension type: [$p dimType]"
     puts "dimension: [$p startDim]:[$p endDim]"
-
-    puts ""
-    puts "identifiers:"
-    foreach i $ident_list {
-        puts $i
-    }
-    puts "--------------------------"
-}
-
-
-set ports2 [$module2 get_ports]
-
-puts "\n\n"
-puts "module2 ports!"
-puts $ports2
-foreach p $ports2 {
-    puts "full port string: [$p string]"
-    puts "port [$p portType] [$p direction] [$p dataType]"
-    set ident_list [$p identifiers]
-
-    puts "dimension: [$p startDim]:[$p endDim]"
-
-    puts ""
-    puts "identifiers:"
-    foreach i $ident_list {
-        puts $i
-    }
     puts "--------------------------"
 }
