6d071705b7174304f0aa1e1f7b332d47fbd34b33
Ben Stahl <48197017+benastahl@users.noreply.github.com>
2025-05-14 11:05:33 -0400

    barcelona update

--- tclslang.cpp
+++ tclslang.cpp
@@ -2,20 +2,17 @@
 // Created by ec2-user on 12/29/24.
 //
 
+// TCLSLANG BY BEN STAHL
+// https://github.com/benastahl/tclslang
+
 #include "slang/syntax/SyntaxTree.h"
 #include "slang/syntax/SyntaxNode.h"
-#include <slang/ast/symbols/PortSymbols.h>
-#include <slang/ast/types/Type.h>
-#include <slang/ast/types/AllTypes.h>
-#include <slang/ast/symbols/CompilationUnitSymbols.h>
-#include <slang/ast/Compilation.h>
 #include <slang/ast/symbols/InstanceSymbols.h>
-#include <iostream>
 #include <cstring>
 #include <string>
 #include <utility>
 #include <tcl.h>
-#include <slang/syntax/AllSyntax.h>
+#include <tclslang/hdl_tree.h>
 
 using namespace slang::syntax;
 using namespace slang::ast;
@@ -25,24 +22,9 @@ int Tree_MethodCmd(ClientData clientData, Tcl_Interp *interp, int argc, const ch
 int Module_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]);
 int Port_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]);
 int Cell_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]);
-int Pin_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]);
+int PortConn_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]);
 void printInstanceTree(const InstanceSymbol* rootInst, int depth);
-class Instance;
-class Port;
-class Module;
-class Tree;
-class Cell;
-class Pin;
 
-unordered_map<string, unique_ptr<Port>> ports;
-unordered_map<string, unique_ptr<Module>> modules;
-unordered_map<string, unique_ptr<Tree>> trees;
-unordered_map<string, unique_ptr<Cell>> cells;
-unordered_map<string, unique_ptr<Pin>> pins;
-
-static int cellCounter = 0;
-static int portCounter = 0;
-static int pinCounter = 0;
 
 /*
 module foo ;
@@ -59,315 +41,13 @@ mod get_ports -> {port_inst port2_inst}
 mod get_cells -> {instance1_inst instance2_inst}
 inst get_pins -> {pin_inst1 pin_inst2}
 pin get_nets  -> {net_name1}
-*/
 
-// Just adds port to list of port instances for use in tcl.
-void addPort(const PortSymbol* portSymbol, string* handleStr) {
+ NEW:
+mod get_cells -> {instance1_inst instance2_inst}
+inst get_connections -> {conn_inst1 conn_inst2}
+ conn_inst get_driver -> driver_inst
+ */
 
-    const Symbol *internal = portSymbol->internalSymbol;
-    if (!internal) {
-        std::cout << "Internal symbol is null" << std::endl;
-        return;
-    }
-
-    // const PortSymbol* port, string portType, string decType, string direction, int startDim, int endDim
-    auto port = make_unique<Port>(portSymbol);
-
-    // Generate a unique handle
-    *handleStr = "port" + std::to_string(portCounter++);
-
-    // Store the instance in the map
-    ports[*handleStr] = std::move(port);
-}
-
-class Pin {
-public:
-    const PortConnection* pin = nullptr;
-    bool netIsPort = false;
-
-    explicit Pin(const PortConnection* pin) {
-        this->pin = pin;
-    }
-
-    // returns either port inst, constant (3, 4), or signal name (if not constant, or symbol).
-    void getNet(string* netStrPtr) {
-        cout << "[Note] Getting net expression...\n";
-
-        // gets net expression
-        const Expression* expr = this->pin->getExpression();
-
-        // checking for valid expression.
-        if (!expr) {
-            cout << "[Warning] No net expression connected to port.\n";
-            return;
-        } else if (expr->bad()) {
-            cout << "[Error] Net expression is invalid or has syntax errors\n";
-            return;
-        }
-
-        const auto& constant = expr->constant;  // checks if net is a constant value (int, string, byte, etc).
-
-        if (constant) {
-            *netStrPtr = constant->toString();  // sets net to constant as string.
-            return;
-        }
-
-        cout << "[Note] Net expression is not a constant.\n";
-
-        // if expr (symbol) is not a port, just returns the name of the symbol (likely signal name).
-        const Symbol* symb = expr->getSymbolReference();
-        if (symb->kind != SymbolKind::Port) {
-            cout << "[Note] Net expression is not a port.\n";
-            *netStrPtr = symb->name;
-            return;
-        }
-
-        // net expr is a port. create port instance to use in tcl and return handle.
-        cout << "[Note] Net expression is a port.\n";
-        this->netIsPort = true;
-        const auto& portSymb = symb->as<PortSymbol>();
-
-        string portHandle;
-        addPort(&portSymb, &portHandle);  // adds port handle to tcl commands
-
-        *netStrPtr = portHandle;  // set net string to port handle tcl command
-    }
-};
-
-class Port {
-public:
-    const PortSymbol *port;
-    string portType; // var or net
-    string direction;
-    string decType;
-    string dimType;
-    vector<array<int, 2>> dimensions;
-
-    explicit Port(const PortSymbol *portSymbol) {
-        this->port = portSymbol;
-
-        cout << "Port Name: " << portSymbol->name << endl;
-
-        this->direction = toString(portSymbol->direction);
-        this->portType = toString(portSymbol->internalSymbol->kind);
-
-        // Determine if it's wire or reg
-        if (portSymbol->isNetPort() || portSymbol->direction == ArgumentDirection::In ||
-            portSymbol->direction == ArgumentDirection::InOut) {
-            this->decType = "wire";
-        } else {
-            this->decType = "reg";
-        }
-
-        // Get the port's type and check if it's an array
-        const Type *type = &portSymbol->getType();
-
-        while (type->isArray()) {
-            if (type->isPackedArray()) {
-                const auto &packedArray = type->getCanonicalType().as<PackedArrayType>();
-                cout << "  Packed array range: " << packedArray.range.left << " to " << packedArray.range.right
-                     << endl;
-                this->dimensions.push_back({packedArray.range.left, packedArray.range.right});
-
-                type = &packedArray.elementType;
-            } else if (type->isUnpackedArray()) {
-                const auto &unpackedArray = type->getCanonicalType().as<FixedSizeUnpackedArrayType>();
-                cout << "  Unpacked array range: " << unpackedArray.range.left << " to "
-                     << unpackedArray.range.right << endl;
-                this->dimensions.push_back({unpackedArray.range.left, unpackedArray.range.right});
-
-                type = &unpackedArray.elementType;
-            } else {
-                cout << "  Unknown array type." << endl;
-                break;
-            }
-        }
-    }
-
-};
-
-class Instance {
-public:
-    string name;
-    const InstanceSymbol* instSymbol = nullptr;
-
-    explicit Instance(const InstanceSymbol* instSymbol) {
-        this->instSymbol = instSymbol;
-        this->name = string(this->instSymbol->name);
-    }
-
-    vector<string> getPorts() const {
-        vector<string> portHandles;
-
-        for (const auto &portOpt: this->instSymbol->body.getPortList()) {
-            const auto &portSymbol = portOpt->as<PortSymbol>();
-
-            string portHandle;
-            addPort(&portSymbol, &portHandle);
-
-            if (portHandle.empty()) continue;
-
-            portHandles.push_back(portHandle);
-        }
-
-        return portHandles;
-    }
-
-    vector<string> getCells() const {
-        vector<string> cellHandles;
-
-        for (const InstanceSymbol &instSymb: this->instSymbol->body.membersOfType<InstanceSymbol>()) {
-            auto cell = make_unique<Cell>(&instSymb);
-
-            // Generate a unique handle for this instance
-            std::string handleStr = "cell" + std::to_string(cellCounter++);
-
-            // Store the instance in the map
-            cells[handleStr] = std::move(cell);
-
-            cellHandles.push_back(handleStr);
-        }
-
-        return cellHandles;
-    }
-
-};
-
-class Cell : public Instance {
-public:
-
-    explicit Cell(const InstanceSymbol* instSymbol) : Instance(instSymbol) {}
-
-    vector<string> getPins() {
-        vector<string> pinHandles;
-        for (const PortConnection* pc : instSymbol->getPortConnections()) {
-            cout << "got port " << pc->port.name << "of kind " << toString(pc->port.kind) << " connected to " << \
-             pc->getExpression() << " of kind " << toString(pc->getExpression()->kind) << endl;
-
-            auto pin = make_unique<Pin>(pc);
-
-            // Generate a unique handle
-            string handleStr = "pin" + std::to_string(pinCounter++);
-
-            // Store the instance in the map
-            pins[handleStr] = std::move(pin);
-
-            pinHandles.push_back(handleStr);
-        }
-
-        return pinHandles;
-    }
-
-};
-
-class Module : public Instance {
-public:
-    explicit Module(const InstanceSymbol* instSymbol) : Instance(instSymbol) {}
-};
-
-void printInstanceTree(const InstanceSymbol* rootInst, int depth = 1) {
-    std::string indent(depth, '\t');
-
-    if (depth == 1) {
-        cout << "InstanceSymbol:"
-             << "\n" << "\tName: " << rootInst->name
-             << "\n" << "\tKind: " << rootInst->kind;
-
-        cout << "\n" << "\tPort List: ";
-        for (const Symbol* port : rootInst->body.getPortList()) {
-            cout << port->name << " ";
-        }
-        cout << "\n";
-
-        cout << "\tPort Connections:";
-        for (const PortConnection* portConn : rootInst->getPortConnections()) {
-            cout << " (" << portConn->port.name << " -> ";
-            if (auto* expr = portConn->getExpression()) {
-                if (auto* symRef = expr->getSymbolReference()) {
-                    cout << symRef->name;
-                } else {
-                    cout << "unresolved";
-                }
-            } else {
-                cout << "unconnected";
-            }
-            cout << ") ";
-        }
-        cout << "\n";
-    }
-
-    for (const InstanceSymbol& inst : rootInst->body.membersOfType<InstanceSymbol>()) {
-        cout << indent << "InstanceSymbol:"
-             << "\n" << indent << "\tName: " << inst.name
-             << "\n" << indent << "\tKind: " << inst.kind;
-
-        cout << "\n" << indent << "\tPort List: ";
-        for (const Symbol* port : inst.body.getPortList()) {
-            cout << port->name << " ";
-        }
-        cout << "\n";
-
-        cout << indent << "\tPort Connections:";
-        for (const PortConnection* portConn : inst.getPortConnections()) {
-            cout << " (" << portConn->port.name << " -> ";
-            if (auto* expr = portConn->getExpression()) {
-                if (auto* symRef = expr->getSymbolReference()) {
-                    cout << symRef->name;
-                } else {
-                    cout << "unresolved";
-                }
-            } else {
-                cout << "unconnected";
-            }
-            cout << ") ";
-        }
-        cout << "\n";
-
-        printInstanceTree(&inst, depth + 1);
-    }
-}
-
-class Tree {
-public:
-
-    shared_ptr<SyntaxTree> tree;
-
-    explicit Tree(const shared_ptr<SyntaxTree> tree) {
-        this->tree = tree;
-    }
-
-    optional<string> getModule(const string& moduleName) const {
-
-        slang::ast::Compilation compilation;
-        compilation.addSyntaxTree(tree);
-        const auto& root = compilation.getRoot();
-
-        // find topmost module instance
-        const Symbol *myModuleFind = root.find(moduleName);
-        if (myModuleFind == nullptr) {
-            cout << "Failed to find module with given name." << endl;
-            return nullopt;
-        }
-
-        const auto& myModule = myModuleFind->as<InstanceSymbol>();
-
-        printInstanceTree(&myModule);
-
-        cout << "got instance: " << myModule.name << " of type " << toString(myModule.kind) << endl;
-
-        auto mod = make_unique<Module>(&myModule);
-
-        // Generate a unique handle for this instance
-        static int modCounter = 0;
-        std::string handleStr = "mod" + std::to_string(modCounter++);
-
-        // Store the instance in the map
-        modules[handleStr] = std::move(mod);
-
-        return handleStr.c_str();
-    };
-
-};
 
 int SlangParse(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]) {
     if (argc < 2) {
@@ -520,6 +200,8 @@ int Module_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const 
 
         Tcl_SetObjResult(interp, Tcl_NewListObj(cells_size, cellsObjForm));
         return TCL_OK;
+    } else if (method == "print_tree") {
+
     }
 
     Tcl_SetResult(interp, const_cast<char*>("Unknown method"), TCL_STATIC);
@@ -642,26 +324,26 @@ int Cell_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const ch
 
         Tcl_SetObjResult(interp, Tcl_NewListObj(ports_size, portsObjForm));
         return TCL_OK;
-    } else if (method == "get_pins") {
+    } else if (method == "get_connections") {
         if (argc != 2) {
-            Tcl_SetResult(interp, const_cast<char*>("Usage: <cell_name> get_pins"), TCL_STATIC);
+            Tcl_SetResult(interp, const_cast<char*>("Usage: <cell_name> get_connections"), TCL_STATIC);
             return TCL_ERROR;
         }
 
         // collect cells
-        vector<string> pinHandles = cell->getPins();
+        vector<string> connHandles = cell->getPortConns();
 
         // convert cells to Tcl_StringObj
-        const unsigned int pins_size = pinHandles.size();
-        Tcl_Obj* cellsObjForm[pins_size];
-        for (int i=0; i < pins_size; i++) {
-            const string pinHandle = pinHandles[i];
-            cellsObjForm[i] = Tcl_NewStringObj(pinHandle.c_str(), pinHandle.length());
+        const unsigned int conns_size = connHandles.size();
+        Tcl_Obj* cellsObjForm[conns_size];
+        for (int i=0; i < conns_size; i++) {
+            const string connHandle = connHandles[i];
+            cellsObjForm[i] = Tcl_NewStringObj(connHandle.c_str(), connHandle.length());
 
-            Tcl_CreateCommand(interp, pinHandle.c_str(), Pin_MethodCmd, (ClientData)&cells[pinHandle], nullptr);
+            Tcl_CreateCommand(interp, connHandle.c_str(), PortConn_MethodCmd, (ClientData)&connections[connHandle], nullptr);
         }
 
-        Tcl_SetObjResult(interp, Tcl_NewListObj(pins_size, cellsObjForm));
+        Tcl_SetObjResult(interp, Tcl_NewListObj(conns_size, cellsObjForm));
         return TCL_OK;
     } else if (method == "get_cells") {
         if (argc != 2) {
@@ -676,7 +358,7 @@ int Cell_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const ch
         const unsigned int cells_size = cellHandles.size();
         Tcl_Obj* cellsObjForm[cells_size];
         for (int i=0; i < cells_size; i++) {
-            const string cellHandle = cellHandles[i];
+            const string& cellHandle = cellHandles[i];
             cellsObjForm[i] = Tcl_NewStringObj(cellHandle.c_str(), cellHandle.length());
 
             Tcl_CreateCommand(interp, cellHandle.c_str(), Cell_MethodCmd, (ClientData)&cells[cellHandle], nullptr);
@@ -690,9 +372,9 @@ int Cell_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const ch
     return TCL_ERROR;
 }
 
-int Pin_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]) {
+int PortConn_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]) {
     if (argc < 2) {
-        Tcl_SetResult(interp, const_cast<char *>("Usage: <pin_name> <method> [args...]"), TCL_STATIC);
+        Tcl_SetResult(interp, const_cast<char *>("Usage: <connection_name> <method> [args...]"), TCL_STATIC);
         return TCL_ERROR;
     }
 
@@ -700,52 +382,82 @@ int Pin_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const cha
     std::string handle = argv[0];
 
     // Find the corresponding Module instance
-    auto it = pins.find(handle);
-    if (it == pins.end()) {
-        Tcl_SetResult(interp, const_cast<char *>("Invalid pin handle"), TCL_STATIC);
+    auto it = connections.find(handle);
+    if (it == connections.end()) {
+        Tcl_SetResult(interp, const_cast<char *>("Invalid connection handle"), TCL_STATIC);
         return TCL_ERROR;
     }
 
-    auto &pin = it->second;
+    auto &connection = it->second;
 
     // handle method called
     std::string method = argv[1];
-    if (method == "name") {
+    if (method == "get_port") {
         if (argc != 2) {
-            Tcl_SetResult(interp, const_cast<char*>("Usage: <pin_name> name"), TCL_STATIC);
+            Tcl_SetResult(interp, const_cast<char*>("Usage: <connection_handle> get_port"), TCL_STATIC);
             return TCL_ERROR;
         }
 
-        Tcl_SetObjResult(interp, Tcl_NewStringObj(string(pin->pin->port.name).c_str(), pin->pin->port.name.size()));
+        Tcl_CreateCommand(interp, net.c_str(), Port_MethodCmd, (ClientData)&connections[net], nullptr);
+
+        Tcl_SetObjResult(interp, Tcl_NewStringObj(string(connection->portConn->port.name).c_str(), connection->portConn->port.name.size()));
         return TCL_OK;
-    } else if (method == "get_net") {
+    } else if (method == "get_driver") {
         if (argc != 2) {
-            Tcl_SetResult(interp, const_cast<char*>("Usage: <pin_name> get_net"), TCL_STATIC);
+            Tcl_SetResult(interp, const_cast<char*>("Usage: <connection_handle> get_driver"), TCL_STATIC);
             return TCL_ERROR;
         }
 
-        string net; // can either be constant, signal name, or port instance handle
-        pin->getNet(&net);
+        string net; // can either be constant, signal name, or net instance handle
+        connection->getNet(&net);
 
-        if (pin->netIsPort) {
-            Tcl_CreateCommand(interp, net.c_str(), Port_MethodCmd, (ClientData)&pins[net], nullptr);
+        if (connection->netIsVar) {
+            Tcl_CreateCommand(interp, net.c_str(), Port_MethodCmd, (ClientData)&connections[net], nullptr);
         }
 
         Tcl_SetObjResult(interp, Tcl_NewStringObj(net.c_str(), net.length()));
-        return TCL_OK;
-    } else if (method == "is_port") {
-        if (argc != 2) {
-            Tcl_SetResult(interp, const_cast<char*>("Usage: <pin_name> is_port"), TCL_STATIC);
-            return TCL_ERROR;
-        }
-
-        Tcl_SetObjResult(interp, Tcl_NewBooleanObj(pin->netIsPort));
         return TCL_OK;
     }
 
     Tcl_SetResult(interp, const_cast<char*>("Unknown method"), TCL_STATIC);
     return TCL_ERROR;
 }
+
+int Driver_MethodCmd(ClientData clientData, Tcl_Interp* interp, int argc, const char* argv[]) {
+    if (argc < 2) {
+        Tcl_SetResult(interp, const_cast<char *>("Usage: <driver_handle> <method> [args...]"), TCL_STATIC);
+        return TCL_ERROR;
+    }
+
+    // Get the handle name (e.g., "module0")
+    std::string handle = argv[0];
+
+    // Find the corresponding Module instance
+    auto it = drivers.find(handle);
+    if (it == drivers.end()) {
+        Tcl_SetResult(interp, const_cast<char *>("Invalid driver handle"), TCL_STATIC);
+        return TCL_ERROR;
+    }
+
+    auto &driver = it->second;
+
+
+    // handle method called
+    std::string method = argv[1];
+    if (method == "name") {
+        if (argc != 2) {
+            Tcl_SetResult(interp, const_cast<char*>("Usage: <driver_handle> name"), TCL_STATIC);
+            return TCL_ERROR;
+        }
+
+        Tcl_SetObjResult(interp, Tcl_NewStringObj(string(driver->).c_str(), connection->portConn->port.name.size()));
+        return TCL_OK;
+    }
+
+    Tcl_SetResult(interp, const_cast<char*>("Unknown method"), TCL_STATIC);
+    return TCL_ERROR;
+}
+
 
 // Initialization function required by Tcl
 extern "C" [[maybe_unused]] int Tclslang_Init(Tcl_Interp* interp) {
